bf535e59a4b4c8635bcf9fd6fe5e31c9c2cc68a8
NateDKing <natedavisking@gmail.com>
2020-05-05 17:33:15 -0500

    5/5/2020

--- Nate Practice/src/main/java/frc/robot/Robot.java
+++ Nate Practice/src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.feedbackDevices.ShuffleBoard;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -21,6 +22,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   public static RobotContainer robotContainer;
+  public static ShuffleBoard shuffleBoard = new ShuffleBoard();
 
   /**
    * This function is run when the robot is first started up and should be used for any
